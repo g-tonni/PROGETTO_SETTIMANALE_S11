904173720d8b8c906523a2e528d34c8668877ec7
Giada <tonni.giada.210802@gmail.com>
2025-12-05 09:17:45 +0100

    FILE BASE INSERITI

---
+++ progetto_settimanale_s11/src/App.jsx
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Provider } from 'react-redux'
+import store from './redux/store'
+
+function App() {
+  return (
+    <Provider store={store}>
+      <p className="text-danger">Ciao</p>
+    </Provider>
+  )
+}
+
+export default App
